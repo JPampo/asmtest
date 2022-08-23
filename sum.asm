c7332450f950726d91ef4ffe4bcc814cf5ba7f03
MAgalDI02 <102761482+MAgalDI02@users.noreply.github.com>
2022-08-23 14:41:05 -0300

    Add files via upload

---
+++ sum.asm
@@ -0,0 +1,26 @@
+.model small
+
+.code 
+main proc
+    mov ah,1
+    int 21h
+    
+    sub al,30h
+    mov dl,al
+    int 21h
+    
+    sub al,30h
+    add dl,al 
+    
+    add dl,30h
+    mov ah,02
+    int 21h
+    
+    mov ah,4ch
+    int 21h
+main endp
+end main
+    
+    
+    
+     
