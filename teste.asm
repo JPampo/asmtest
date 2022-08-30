1a112c23293e7c5943297091729cf7481043dc94
OzneeBR <111551100+OzneeBR@users.noreply.github.com>
2022-08-30 14:14:17 -0300

    Update teste.asm

--- teste.asm
+++ teste.asm
@@ -12,10 +12,56 @@
             int 21h
             mov dl, bl
             add dl, 1
+            add bl, 1
             INC CL
-            CMP CL, 26 
-            JNZ VOLTA 
+            CMP CL, 10 
+            JNZ VOLTA
+        mov ah, 07
+        int 21h
+        mov dl, bl
+        mov ah, 02
+        mov cl, 0
+        CARLOS: 
+            int 21h
+            mov bl, dl
+            mov dl, 10
+            int 21h
+            mov dl, bl
+            add dl, 1
+            INC CL
+            CMP CL, 10 
+            JNZ CARLOS
+         mov ah, 07
+         int 21h
+         mov dl, bl
+         mov ah, 02
+         mov cl, 0
+         BRENO: 
+            int 21h
+            mov bl, dl
+            mov dl, 10
+            int 21h
+            mov dl, bl
+            add dl, 1
+            INC CL
+            CMP CL, 7 
+            JNZ BRENO
+         mov ah, 07
+         int 21h
+         mov ah, 02
+        
+           
+        mov cl, 0
+        mov dl, 48
+        
+        CESAR:
+            int 21h
+            mov bl, dl
+            add dl, 1
+            INC CL
+            CMP CL, 10
+            JNZ CESAR
+        
+        
     main endp
 end main
- 
-    
